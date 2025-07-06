824261034ce8cc63e55af7b34839ba43fb727c42
kopalych <kopalychmail@gmail.com>
2025-07-07 01:51:04 +0200

    ex00: Header protection

--- ex00/include/whatever.hpp
+++ ex00/include/whatever.hpp
@@ -6,9 +6,12 @@
 /*   By: akostian <akostian@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 21:36:33 by akostian          #+#    #+#             */
-/*   Updated: 2025/07/06 02:03:43 by akostian         ###   ########.fr       */
+/*   Updated: 2025/07/07 01:47:32 by akostian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
 
 template <typename T>
 void	swap(T &a, T &b)
@@ -35,3 +38,5 @@ const T &max(const T &a, const T &b)
 		return a;
 	return b;
 }
+
+#endif // WHATEVER_HPP
